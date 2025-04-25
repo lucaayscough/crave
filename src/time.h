cf5bc9e318970e9af058194415369ada043e4e84
Robin Leathart <56587304+rleathart@users.noreply.github.com>
2025-04-25 17:42:07 +0100

    Fix time.h compile error
    As a side effect of adding ./src to the include path, our time.h file
    conflicts with the system time.h. Using #include_next is a hacky
    workaround for now but it's probably a good idea to rename our time.h
    file at some point in the future.

--- src/time.h
+++ src/time.h
@@ -1,6 +1,9 @@
 #ifndef TIME_H
 #define TIME_H
 
+#include <stdio.h>
+#include_next <time.h>
+
 void print_runtime_ms(clock_t start) {
   clock_t end = clock();
   double runtime = (double)(end - start) / CLOCKS_PER_SEC * 1000.0;
