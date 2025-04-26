664e82b10c97e4ef03e57e3f1185fb5764d7e53a
lucaayscough <lucaayscough@gmail.com>
2025-04-26 18:19:43 +0100

    move to stb-style single header file

--- scripts/generate_rand_input.py
+++ scripts/generate_rand_input.py
@@ -6,6 +6,7 @@ import struct
 import torch
 import argparse
 import time
+import torchaudio
 
 def tensor_to_file(name: str, x: torch.Tensor):
   ndim = x.ndim
@@ -31,12 +32,22 @@ def tensor_to_file(name: str, x: torch.Tensor):
     f.write(header_buf + metadata_buf + flat_buf)
 
 if __name__ == "__main__":
-  x = torch.arange(0, 16).reshape(2, 2, -1)
-  y = x.flip(1)
+  x = torchaudio.load("/Users/lucaayscough/Music/Sound/bjork_hunter_80bpm_stereo.wav")
+  x = x[0][0]
+  x = x[:2048*2048]
+  x = x.reshape(-1, 1, 2048)
 
-  print(x)
-  print(y)
+  y = torch.zeros(1, 1, 2048)
 
-  tensor_to_file("x_test", x)
-  tensor_to_file("y_test", y)
+  model = torch.jit.load("/Users/lucaayscough/Desktop/rave/runs/mrbill-mob-v2-nonoise-cap16/mrbill-mob-v2-nonoise-cap16_4e3d996d79/version_0/checkpoints/mrbill-mob-v2-nonoise-cap16_4e3d996d79.ts")
+  model.eval()
+
+  for x_ in x:
+    x_ = x_.unsqueeze(0)
+    y = torch.cat((y, model.forward(x_)))
+
+  y = y / torch.max(torch.abs(y))
+  y = (y * 32767).to(torch.int16)
+  y = y.reshape(1, -1).detach().numpy()
+  torchaudio.save("audio.wav", y, 44100)
 
--- src/main.c
+++ src/main.c
@@ -1,30 +1,30 @@
+#define CRAVE_IMPLEMENTATION
 #include "crave.h"
 #include "models/v1.h"
 
 int main(int argc, char* argv[]) {
   arena_t arena = {};
   arena_init(&arena, 5 * MB);
-
   v1_model_weights_t weights = {};
   load_weights(&arena, &weights);
+  //tensor_t* z = tensor_load_from_file(&arena, BIN_PATH"z.bin", 4096);
+  //assert(z != NULL);
+  //tensor_t* zr = tensor_load_from_file(&arena, BIN_PATH"zr.bin", TENSOR_AUTO_CAP);
+  //assert(zr != NULL);
+  //decode(z, &weights);
+  //tensor_print_error_stats(z, zr);
 
   clock_t start = clock();
-
   tensor_t* z = tensor_create(&arena, U32_TPL(1, 4, 1), 4096);
-
   for (int i = 0; i < 16; ++i) {
     tensor_init(z, U32_TPL(1, 4, 1));
-
-    z->data[0] = 0.1f;
-    z->data[1] = 0.2f;
-    z->data[2] = 0.3f;
-    z->data[3] = 0.4f;
-
+    z->data[0] = 5.1f;
+    z->data[1] = 5.2f;
+    z->data[2] = 5.3f;
+    z->data[3] = 5.4f;
     decode(z, &weights);
   }
-
   print_runtime_ms(start);
   arena_free(&arena);
-
   return 0;
 }
